fa8dadebcfebce671a66b6859600fd716ab75d07
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2024-09-18 14:18:07 +0700

    fix cors

--- ToyPlatform_SWP_Project/src/main/java/com/example/toyplatform_swp_project/security/SecurityConfig.java
+++ ToyPlatform_SWP_Project/src/main/java/com/example/toyplatform_swp_project/security/SecurityConfig.java
@@ -1,9 +1,9 @@
-package com.example.toyplatform_swp_project.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-}
+//package com.example.toyplatform_swp_project.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//}
